2a58eac2b249b8835bbbf7d6768d8b982d769349
yelekrishnababu <69049531+yelekrishnababu@users.noreply.github.com>
2022-10-13 14:40:55 +0530

    Update local_azure_sqldb.py

--- local_azure_sqldb.py
+++ local_azure_sqldb.py
@@ -3,7 +3,7 @@ import pyodbc
 server = 'sqlserver45.database.windows.net'
 database = 'ky2910'
 username = 'ky2910'
-password = 'Krishna@2000'
+password = 'XXXxxxx'
 driver= '{SQL Server}'
 conn = pyodbc.connect('Driver='+driver+';Server=tcp:'+server+',1433;Database='+database+';Uid='+username+';PWD='+ password)
 cursor = conn.cursor()
